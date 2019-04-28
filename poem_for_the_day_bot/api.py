b24bb19da21d9ad1532af798de67612166085ca0
ivan <ivan.h@smart-gamma.com>
2019-04-28 09:19:23 +0300

    get poem on index page

--- poem_for_the_day_bot/api.py
+++ poem_for_the_day_bot/api.py
@@ -1,6 +1,8 @@
-from aiohttp import web
 import os
+from aiohttp import web
+from .poem import get_poem_today
 
 
 async def index(request):
-    return web.Response(text=os.environ['MESSAGE'])
+    poem = await get_poem_today()
+    return web.Response(text=poem)
